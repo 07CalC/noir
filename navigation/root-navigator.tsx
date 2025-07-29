c9a039dc0d042edbdfff1f50eb0694d23af2e387
Sohan Prabhas <sohannaidu040@gmail.com>
2025-07-29 20:09:38 +0530

    Added onboarding screen with dark mode and navigation

--- navigation/root-navigator.tsx
+++ navigation/root-navigator.tsx
@@ -1,20 +1,29 @@
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import TestScreen from "screens/test-screen";
-
+import OnboardingScreen from "../screens/OnboardingScreen";
+import LoginScreen from "../screens/LoginScreen"; 
 
 const Stack = createStackNavigator();
 
-
-
 export default function RootNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="onboarding">
+      <Stack.Screen
+        name="onboarding"
+        component={OnboardingScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="test"
         component={TestScreen}
         options={{ headerShown: false }}
       />
+      <Stack.Screen
+        name="login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
-
